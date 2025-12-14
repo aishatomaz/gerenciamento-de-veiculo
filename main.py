7a611f866b7460fde3d9bd042c0dbd7633599252
Aisha <aisha.tomaz@aluno.ufca.edu.br>
2025-12-14 01:07:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+import json
 from datetime import date
 from dominio.veiculo import Veiculo
 from dominio.motorista import Motorista
